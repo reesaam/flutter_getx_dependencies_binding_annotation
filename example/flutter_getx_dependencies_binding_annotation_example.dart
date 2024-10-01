8a604d58f9797986ccbedf8f245c94181168561b
Resam Taghipour <resam.t@gmail.com>
2024-10-01 08:09:25 +0330

    Project Initialization

---
+++ example/flutter_getx_dependencies_binding_annotation_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_getx_dependencies_binding_annotation/flutter_getx_dependencies_binding_annotation.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_getx_dependencies_binding_annotation_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_getx_dependencies_binding_annotation/flutter_getx_dependencies_binding_annotation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
