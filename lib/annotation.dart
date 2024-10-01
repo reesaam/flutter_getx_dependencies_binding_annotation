9702b401db734f3f1dcf5ebc30d56c781ab25bbe
Resam Taghipour <resam.t@gmail.com>
2024-10-01 09:02:55 +0330

    Commit

--- lib/annotation.dart
+++ lib/annotation.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+
 library;
 
 export 'src/base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
