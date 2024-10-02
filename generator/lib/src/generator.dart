90df84673b24cc7bf687cf714e85831d54c2ace5
Resam Taghipour <resam.t@gmail.com>
2024-10-02 19:04:10 +0330

    Commit Work

--- generator/lib/src/generator.dart
+++ generator/lib/src/generator.dart
@@ -5,13 +5,30 @@ import 'package:getx_dependencies_binding_annotation/annotation.dart';
 import 'package:source_gen/source_gen.dart';
 
 class GetXDependenciesBindingGenerator extends GeneratorForAnnotation<GetPut> {
+
+  String code = '///Not Found';
+
   @override
-  FutureOr<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
+  FutureOr<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) async {
 
-    print(element);
-    print(annotation);
-    print(buildStep);
+    String? command;
 
-    return "// Hey! Annotation found!";
+    if (element.metadata.first.element != null && element.declaration != null && element.declaration!.name != null && element.declaration!.name!.isNotEmpty) {
+      switch(element.metadata.first.element!.name) {
+        case 'page': command = _addPage(element.declaration!.name!); break;
+        case 'controller': command = _addController(element.declaration!.name!); break;
+        case 'component': command = _addComponent(element.declaration!.name!); break;
+        case 'repository': command = _addRepository(element.declaration!.name!); break;
+        default: break;
+      }
+    }
+    return code;
   }
+
+  _addPage(String page) => 'GetPage(name: /${page}Route, page: ${page}.new)';
+  _addController(String controller) => 'Get.lazyPut<${controller}>(() => ${controller}(), fenix: true);';
+  _addComponent(String controller) => 'Get.lazyPut<${controller}>(() => ${controller}(), fenix: true);';
+  _addRepository(String controller) => 'Get.lazyPut<${controller}>(() => ${controller}(), fenix: true);';
+
+  _addLine(String line) => code = '\n$line';
 }
