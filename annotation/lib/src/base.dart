f56bba3664176b468c2f29ad611887ce09ccab79
Resam Taghipour <resam.t@gmail.com>
2024-10-07 17:56:13 +0330

    Annotation

--- annotation/lib/src/base.dart
+++ annotation/lib/src/base.dart
@@ -5,10 +5,13 @@ import 'enums.dart';
 
 class GetPut {
   final String? as;
+  final String? route;
+  final bool? isInitial;
+  final bool? isUnknown;
   final AnnotationTypes? type;
 
-  const GetPut.page({this.as}) : type = AnnotationTypes.page;
-  const GetPut.controller({this.as}) : type = AnnotationTypes.controller;
-  const GetPut.component({this.as}) : type = AnnotationTypes.component;
-  const GetPut.repository({this.as}) : type = AnnotationTypes.repository;
+  const GetPut.page({this.as, this.route, this.isInitial, this.isUnknown}) : type = AnnotationTypes.page;
+  const GetPut.controller({this.as}) : type = AnnotationTypes.controller, route = null, isInitial = null, isUnknown = null;
+  const GetPut.component({this.as}) : type = AnnotationTypes.component, route = null, isInitial = null, isUnknown = null;
+  const GetPut.repository({this.as}) : type = AnnotationTypes.repository, route = null, isInitial = null, isUnknown = null;
 }
