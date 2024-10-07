44a11894045ed56028a134473554a6088bbfe18e
Resam Taghipour <resam.t@gmail.com>
2024-10-07 19:30:28 +0330

    Generated main.get_put.dart

--- example/lib/main.get_put.dart
+++ example/lib/main.get_put.dart
@@ -4,48 +4,42 @@
 // CodeGenerator
 // **************************************************************************
 
-/// /// GENERATED CODE - DO NOT MODIFY BY HAND EVER
-/// Dependencies Binding Annotation Generator for GetX State Manager
-/// This file will provide mandatory dependencies for "GetMaterialApp"
-///   Including:
-///     - Pages
-///     - Injected Dependencies:
+/// ///  GENERATED CODE - DO NOT MODIFY BY HAND EVER
+
+///  Dependencies binding annotation generator for [ GetX State Manager ]
+///  This file will provide mandatory dependencies for [ GetMaterialApp ]
+///    Including:
+///      - Pages
+///      - Injected Dependencies:
 ///        - Controllers
 ///        - Components
 ///        - Repositories
-///   Package:
-///     Package Last Version: 1.0.0
-///     Package Name: getx_dependencies_binding_annotation_generator
-///     Package Description: Getx Dependencies Binding Annotation Generator
-///     Package Address: ** Will Add in Next Update after First Publish **
-///     Package GitHubRepositoryAddress: https://github.com/reesaam/flutter_getx_dependencies_binding_annotation
-///   Written and Provided by:
-///     Resam Taghipour
-///     Email: resam.t@gmail.com
-///     Website: https://www.resam-t.ir
-///     LinkedIn: https://www.linkedin.com/in/resam
-///     GitHub: https://github.com/reesaam
-///
-///
-///
-///
-///
-///
-///
+
+///  Package:
+///    Package Last Version: [ 1.0.0 ]
+///    Package Name: [ getx_dependencies_binding_annotation_generator ]
+///    Package Description: [ Getx Dependencies Binding Annotation Generator ]
+///    Package Address: [ ** Will Add in Next Update after First Publish ** ]
+///    Package GitHubRepositoryAddress: [ https://github.com/reesaam/flutter_getx_dependencies_binding_annotation ]
+
+///  Written and Provided by:
+///    [ Resam Taghipour ]
+///    Email: [ resam.t@gmail.com ]
+///    Website: [ https://www.resam-t.ir ]
+///    LinkedIn: [ https://www.linkedin.com/in/resam ]
+///    GitHub: [ https://github.com/reesaam ]
 
 import 'main.dart';
 import 'package:get/get.dart';
 import 'components/storage_component.dart';
-import 'features/homepage/data/remote_data_source_repository.dart';
 import 'features/homepage/data/local_data_source_repository.dart';
+import 'features/homepage/data/remote_data_source_repository.dart';
 import 'features/homepage/controller/homepage_controller.dart';
 import 'features/not_found/controller/not_found_controller.dart';
 import 'features/homepage/view/homepage_view.dart';
 import 'features/not_found/view/not_found_view.dart';
 
-/// Generated Library
-/// GetPut Main Library
-/// GetPut Statistics:
+/// Generated Library Statistics:
 ///   Imports Count: 7
 ///   Pages Count: 2
 ///   Controllers Count: 2
@@ -57,6 +51,10 @@ class GetPutPages {
         GetPage(name: '/HomePage', page: HomePage.new),
         GetPage(name: '/NotFoundPage', page: NotFoundPage.new),
       ];
+  static GetPage get initialRoute =>
+      GetPage(name: '/HomePage', page: HomePage.new);
+  static GetPage get unknownRoute =>
+      GetPage(name: '/NotFoundPage', page: NotFoundPage.new);
 }
 
 class GetPutBindings implements Bindings {
@@ -87,11 +85,11 @@ class _GetPutComponent extends Bindings {
 class _GetPutRepository extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<RemoteDataSourceRepository>(
-        () => RemoteDataSourceRepositoryImpl(),
-        fenix: true);
     Get.lazyPut<LocalDataSourceRepository>(
         () => LocalDataSourceRepositoryImpl(),
+        fenix: true);
+    Get.lazyPut<RemoteDataSourceRepository>(
+        () => RemoteDataSourceRepositoryImpl(),
         fenix: true);
   }
 }
