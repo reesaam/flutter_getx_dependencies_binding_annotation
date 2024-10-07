e60319ee098c06fb49837f5b711eadcbc1fcb8e0
Resam Taghipour <resam.t@gmail.com>
2024-10-07 19:30:10 +0330

    Info and Description Generator

--- generator/lib/components/descriptions_generator.dart
+++ generator/lib/components/descriptions_generator.dart
@@ -1,49 +1,54 @@
+import '../resources/info.dart';
+
 class DescriptionGenerator {
   String result = '';
 
-  String generate({String? name}) {
-    _addLine(_caution);
-    _addSpace();
-    _addLine('Dependencies Binding Annotation Generator for GetX State Manager');
-    _addLine('This file will provide mandatory dependencies for \"GetMaterialApp\"');
-    _addLine('  Including:');
-    _addLine('    - Pages');
-    _addLine('    - Injected Dependencies:');
-    _addLine('       - Controllers');
-    _addLine('       - Components');
-    _addLine('       - Repositories');
-    _addBigSpace();
-    _addLine('  Package:');
-    _addLine('    Package Last Version: $_version');
-    _addLine('    Package Name: $_packageName');
-    _addLine('    Package Description: $_packageNameDescription');
-    _addLine('    Package Address: $_packageAddress');
-    _addLine('    Package GitHubRepositoryAddress: $_packageGitHubRepositoryAddress');
-    _addBigSpace();
-    _addLine('  Written and Provided by:');
-    _addLine('    $_authorName');
-    _addLine('    Email: $_emailAddress');
-    _addLine('    Website: $_websiteAddress');
-    _addLine('    LinkedIn: $_linkedinAddress');
-    _addLine('    GitHub: $_githubAddress');
-    _addSpace();
+  String generate({
+    bool? all,
+    bool? caution,
+    bool? authorInfo,
+    bool? packageInfo,
+    bool? description,
+  }) {
+    if (all == true || caution == true) {
+      _addLine(_caution);
+      _addSpace();
+    }
+    if (all == true || description == true) {
+      _addLine('Dependencies binding annotation generator for [ GetX State Manager ]');
+      _addLine('This file will provide mandatory dependencies for [ GetMaterialApp ]');
+      _addLine('  Including:');
+      _addLine('    - Pages');
+      _addLine('    - Injected Dependencies:');
+      _addLine('      - Controllers');
+      _addLine('      - Components');
+      _addLine('      - Repositories');
+      _addBigSpace();
+    }
+    if (all == true || packageInfo == true) {
+      _addLine('Package:');
+      _addLine('  Package Last Version: [ ${PackageAuthorInfo.version} ]');
+      _addLine('  Package Name: [ ${PackageAuthorInfo.packageName} ]');
+      _addLine('  Package Description: [ ${PackageAuthorInfo.packageNameDescription} ]');
+      _addLine('  Package Address: [ ${PackageAuthorInfo.packageAddress} ]');
+      _addLine('  Package GitHubRepositoryAddress: [ ${PackageAuthorInfo.packageGitHubRepositoryAddress} ]');
+      _addBigSpace();
+    }
+    if (all == true || authorInfo == true) {
+      _addLine('Written and Provided by:');
+      _addLine('  [ ${PackageAuthorInfo.authorName} ]');
+      _addLine('  Email: [ ${PackageAuthorInfo.emailAddress} ]');
+      _addLine('  Website: [ ${PackageAuthorInfo.websiteAddress} ]');
+      _addLine('  LinkedIn: [ ${PackageAuthorInfo.linkedinAddress} ]');
+      _addLine('  GitHub: [ ${PackageAuthorInfo.githubAddress} ]');
+      _addBigSpace();
+    }
     return result;
   }
 
-  
   String get _caution => 'GENERATED CODE - DO NOT MODIFY BY HAND EVER';
-  String get _packageName => 'getx_dependencies_binding_annotation_generator';
-  String get _packageNameDescription => 'Getx Dependencies Binding Annotation Generator';
-  String get _packageAddress => '** Will Add in Next Update after First Publish **';
-  String get _packageGitHubRepositoryAddress => 'https://github.com/reesaam/flutter_getx_dependencies_binding_annotation';
-  String get _authorName => 'Resam Taghipour';
-  String get _emailAddress => 'resam.t@gmail.com';
-  String get _websiteAddress => 'https://www.resam-t.ir';
-  String get _linkedinAddress => 'https://www.linkedin.com/in/resam';
-  String get _githubAddress => 'https://github.com/reesaam';
-  String get _version => '1.0.0';
 
-  String _addLine(String line) => result += '/// $line\n';
-  String _addSpace() => '///\n\n';
-  String _addBigSpace() => '///\n\n\n\n';
+  String _addLine(String line) => result += '///  $line\n';
+  String _addSpace() => result += '\n';
+  String _addBigSpace() => result += '\n\n';
 }
